e8fc06b4c2f1cbc8a6d1d55d05cdf691488d1105
iProdigy <gitprodigy@proton.me>
2025-02-13 14:09:04 -0600

    chore: downgrade log level

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -5,6 +5,7 @@ import net.runelite.client.externalplugins.ExternalPluginManager;
 
 public class ExamplePluginTest
 {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception
 	{
 		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
